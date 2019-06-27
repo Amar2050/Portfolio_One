7f5d36051c31e23f9f2f77d2da817cb03be41888
Amar2050 <amarboutayeb@gmail.com>
2019-06-27 19:02:55 +0200

    Adding CSS JS JPG
    Updating html
    adding css sheet
    adding js sheet
    ading picture

---
+++ script.js
@@ -0,0 +1,8 @@
+function dynamicNav() {
+    var x = document.getElementById("NavBarHamburger");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
