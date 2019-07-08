b3749c35b33bc1e2568d10e1b5a6b91867cf7a64
Amar2050 <amarboutayeb@gmail.com>
2019-07-08 14:14:19 +0200

    Adding contactform php file
    // To Do
    adding verification form
    after creating constraint on the form

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+if (issset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "amar2050@hotmail.fr";
+    $headers = "De: ".$mailFrom;
+    $txt = "Un email de ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
