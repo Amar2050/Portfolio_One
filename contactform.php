d23bcf46e91995a5b44edc9a4596ed8796c5ed84
Amar2050 <amarboutayeb@gmail.com>
2019-07-14 22:14:06 +0200

    Near from finishing v1
    Html css and js seem to be done
    checking for on scroll closing nav
    maybe adding an dynamic down arrow just for fun
    and finishing the form on php

--- contactform.php
+++ contactform.php
@@ -4,7 +4,8 @@ if (issset($_POST['submit'])) {
     $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
-
+    
+    // Enter your email , gmail seem doen't work create one with web provider and alias it 
     $mailTo = "email@mail.fr";
     $headers = "De: ".$mailFrom;
     $txt = "Un email de ".$name.".\n\n".$message;
--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function dynamicNav() {
     var x = document.getElementById("NavBarHamburger");
-    if (x.style.display === "block") {
+    if (x.style.display === "block") 
+    {
         x.style.display = "none";
-    } else {
+    } 
+    else 
+    {
         x.style.display = "block";
     }
 }
-// To do : finish the function to show tab by replacing active into tab who is click 
-// function tabsDisplay() {
-//     var tabSelected = document.getElementById("NavBarHamburger");
-    
-// }
+function closingNav() {
+    var x = document.getElementById("NavBarHamburger");
+    if (x.style.display === "block") 
+    {
+        x.style.display = "none";
+    }
+}
